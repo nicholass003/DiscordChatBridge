8f844b09f441791bad8af01a93e1662047d74887
Vega Nicholas S. <veganicholas123@gmail.com>
2024-07-11 16:50:40 +0700

    fix: code style

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -39,6 +39,29 @@ return (new PhpCsFixer\Config())
 			'import_functions' => true,
 			'import_classes' => null,
 		],
+		'header_comment' => [
+			'comment_type' => 'comment',
+			'header' => <<<BODY
+Copyright (c) 2024 - present nicholass003
+       _      _           _                ___   ___ ____
+      (_)    | |         | |              / _ \ / _ \___ \
+ _ __  _  ___| |__   ___ | | __ _ ___ ___| | | | | | |__) |
+| '_ \| |/ __| '_ \ / _ \| |/ _` / __/ __| | | | | | |__ <
+| | | | | (__| | | | (_) | | (_| \__ \__ \ |_| | |_| |__) |
+|_| |_|_|\___|_| |_|\___/|_|\__,_|___/___/\___/ \___/____/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+@author  nicholass003
+@link    https://github.com/nicholass003/
+
+
+BODY,
+			'location' => 'after_open'
+		],
 		'indentation_type' => true,
 		'logical_operators' => true,
 		'native_constant_invocation' => [
